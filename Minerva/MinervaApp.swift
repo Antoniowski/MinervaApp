35dc5d02aa51a5d431487693c1677ea5670f8a5d
Antonio Romano <r97.antonio@gmail.com>
2022-02-11 14:22:48 +0100

    Initial Commit

---
+++ Minerva/MinervaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinervaApp.swift
+//  Minerva
+//
+//  Created by Antonio Romano on 11/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MinervaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
