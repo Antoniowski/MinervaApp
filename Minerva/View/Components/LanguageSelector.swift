f86b802e2a02ebfd228f7ddb4c5db4b3f846e8de
Antonio Romano <r97.antonio@gmail.com>
2022-02-16 16:45:41 +0100

    creato struct selettore

---
+++ Minerva/View/Components/LanguageSelector.swift
@@ -0,0 +1,15 @@
+//
+//  LanguageSelector.swift
+//  Minerva
+//
+//  Created by Antonio Romano on 16/02/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct LanguageSelector: View{
+    var body: some View{
+        Circle()
+    }
+}
