5e4e3419f29ad7595bd1867e8d9e468c526d1854
Antonio Romano <r97.antonio@gmail.com>
2022-02-16 17:04:17 +0100

    Modificato Onboarding

--- Minerva/View/Components/LanguageSelector.swift
+++ Minerva/View/Components/LanguageSelector.swift
@@ -9,7 +9,21 @@ import Foundation
 import SwiftUI
 
 struct LanguageSelector: View{
+    var language: String
     var body: some View{
-        Circle()
+        HStack{
+            Image(language)
+                .resizable()
+                .scaledToFit()
+        }
+        .frame(width: UIScreen.main.bounds.height*0.08, height: UIScreen.main.bounds.height*0.08)
+        .clipped()
+        .clipShape(Circle())
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        LanguageSelector(language: "flagItaly")
     }
 }
